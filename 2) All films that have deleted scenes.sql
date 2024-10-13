626828ce89513a3a6983e610bc77985956476dc2
Abdullah Ghaffar <abdullahghaffar.work@gmail.com>
2024-10-13 20:40:00 +0500

    Query 2

--- 2) All films that have deleted scenes.sql
+++ 2) All films that have deleted scenes.sql
@@ -1,1 +1,9 @@
 -- Query 2) All films that have deleted scenes
+select f.title, f.special_features, f.release_year
+
+
+from sakila.film f
+
+where f.special_features like '%Deleted Scenes%'and title like 'c%'
+
+
