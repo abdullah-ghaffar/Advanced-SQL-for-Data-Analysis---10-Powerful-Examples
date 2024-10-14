84e7026ec0ac0639ef60d341ea7515c91325b241
Abdullah Ghaffar <abdullahghaffar.work@gmail.com>
2024-10-14 21:17:19 +0500

    Query 9

--- 9) Which movies should we discontinue from our catalogue (less than 2 lifetime rentals).sql
+++ 9) Which movies should we discontinue from our catalogue (less than 2 lifetime rentals).sql
@@ -1,8 +1,12 @@
 -- Query 9) Which movies should we discontinue from our catalogue (less than 2 lifetime rentals)
 
-select inventory_id, count(*)
+with low_rentals as (select inventory_id, count(*)
 from rental r
 group by inventory_id
-having count(*)<=1
+having count(*)<=1)
+
+select low_rentals.inventory_id, i.film_id, f.title from low_rentals
+join inventory i on i.inventory_id = low_rentals.inventory_id
+join film f on f.film_id = i.film_id
 
 
